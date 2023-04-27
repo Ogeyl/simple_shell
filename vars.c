c64abf33e6f6df405ed632d69e6e83dad592767e
pweetybubae <ebubeogbuke@outlook.com>
2023-04-26 22:54:08 -0700

    update

--- vars.c
+++ vars.c
@@ -46,44 +46,46 @@ int is_chain(info_t *info, char *buf, size_t *p)
 }
 
 /**
-* check_chain - checks we should continue chaining based on last status
-* @info: the parameter struct
-* @buf: the char buffer
-* @p: address of current position in buf
-* @i: starting position in buf
-* @len: length of buf
-*
-* Return: Void
-*/
+ * check_chain - checks we should continue chaining based on last status
+ * @info: the parameter struct
+ * @buf: the char buffer
+ * @p: address of current position in buf
+ * @i: starting position in buf
+ * @len: length of buf
+ *
+ * Return: Void
+ */
 void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 {
+	size_t j;
+
 	if (buf == NULL || p == NULL)
 	{
-	return; /*Check for null pointers */
+		return; /*Check for null pointers */
 	}
-
-	size_t j = *p;
+	j = *p;
 
 	if (info->cmd_buf_type == CMD_AND)
 	{
-	if (info->status)
+		if (info->status)
 		{
-	buf[i] = 0;
-		j = len;
-	}
+			buf[i] = 0;
+			j = len;
 		}
+	}
 
 	if (info->cmd_buf_type == CMD_OR)
 	{
-	if (!info->status)
+		if (!info->status)
 		{
-	buf[i] = 0;
-		j = len;
-	}
+			buf[i] = 0;
+			j = len;
 		}
+	}
 
 	*p = j;
 }
+
 
 
 /**
